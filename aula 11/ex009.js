4798ed16dcf0e2b86a4405fd7a3f9d9daabba30b
rafaelamachadocamara <rafaelamachado.camara@yahoo.com.br>
2025-02-12 16:31:56 -0300

    criando uma condição composta

---
+++ aula 11/ex009.js
@@ -0,0 +1,9 @@
+var país = 'Brasil';
+
+console.log(`Vivendo em ${país}`);
+
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro'); 
+} else { // condição composta
+    console.log('Você é Estrangeiro');
+}
