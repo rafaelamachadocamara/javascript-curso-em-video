62e4d1ac0e1b8bd1d50e7adf524359cf9342b7ac
rafaelamachadocamara <rafaelamachado.camara@yahoo.com.br>
2025-02-13 23:38:09 -0300

    exercicio para verificação de idade

---
+++ aula 14/script.js
@@ -0,0 +1,50 @@
+function verificar() {
+    var data = new Date();
+    var ano = data.getFullYear();
+    var fano = document.getElementById('txtano');
+    var res = document.getElementById('res');
+
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        alert('[ERRO] Verifique os dados e tente novamente!');
+    } else {
+        var fsex = document.getElementsByName('radsex');
+        var idade = ano - Number(fano.value);
+        var genero = '';
+        var img = document.createElement('img');
+        img.setAttribute('id', 'foto');
+        if (fsex[0].checked) {
+            genero = 'Homem';
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'assets/bebemenino.jpg');
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'assets/jovemhomem.jpg');
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'assets/homemadulto.jpg');
+            } else {
+                //idoso
+                img.setAttribute('src', 'assets/idosohomem.jpg');
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher';
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'assets/bebemenina.jpg');
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'assets/jovemmulher.jpg');
+            } else if (idade < 50) {
+                //adulta
+                img.setAttribute('src', 'assets/mulheradulta.jpg');
+            } else {
+                //idosa
+                img.setAttribute('src', 'assets/idosamulher.jpg');
+            }
+        }
+        res.style.textAlign = 'center';
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`;
+        res.appendChild(img);
+    }
+}
