c7e7bf973cd3987f4a9c1d90c426abca15162347
rafaelamachadocamara <rafaelamachado.camara@yahoo.com.br>
2025-02-14 17:36:46 -0300

    criando laço de repetição

---
+++ aula 15/ambiente.js
@@ -0,0 +1,13 @@
+var contador = 1;
+
+while (contador <= 6) { // repetições
+  console.log("olá mundo!");
+  contador++;
+}
+
+/* console.log('olá mundo!');
+console.log('olá mundo!');
+console.log('olá mundo!');
+console.log('olá mundo!');
+console.log('olá mundo!');
+console.log('olá mundo!'); */
