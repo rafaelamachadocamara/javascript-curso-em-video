096a40460a64b2c155f281c566b23bea8e86ce27
rafaelamachadocamara <rafaelamachado.camara@yahoo.com.br>
2025-02-14 17:45:41 -0300

    testando duas formas de estruturas de repetição

--- aula 15/ambiente.js
+++ aula 15/ambiente.js
@@ -1,9 +1,5 @@
-var contador = 1;
-
-while (contador <= 6) { // repetições
-  console.log(`passo ${contador}`);
-  contador++;
-}
+/* essa estrutura é a mesma coisa que o código abaixo com os consoles.log, 
+ela é uma repetição de um bloco de código,  
 
 /* console.log('olá mundo!');
 console.log('olá mundo!');
@@ -11,3 +7,19 @@ console.log('olá mundo!');
 console.log('olá mundo!');
 console.log('olá mundo!');
 console.log('olá mundo!'); */
+
+var contador = 1;
+
+// aqui iremos testar primeiro e executar depois
+
+while (contador <= 6) {
+  // repetições
+  console.log(`passo ${contador}`);
+  contador++;
+}
+
+do {
+  // aqui iremos executar primeiro e depois testar depois
+  console.log(`passo ${contador}`);
+  contador++;
+} while (contador <= 6);
