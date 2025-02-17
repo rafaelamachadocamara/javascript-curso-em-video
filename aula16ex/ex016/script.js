f457d465c2e3f0ad12915e218201df2f7c0ff79c
rafaelamachadocamara <rafaelamachado.camara@yahoo.com.br>
2025-02-17 18:45:39 -0300

    concatenando meu nome

--- aula16ex/ex016/script.js
+++ aula16ex/ex016/script.js
@@ -1,3 +1,11 @@
+function nome() {
+    var n = 'Rafaela';
+    var nomeRes = document.getElementById('nomeRes');
+    if (nomeRes) {
+        nomeRes.innerHTML = `Desenvolvido por: ${n}`;
+    }
+}
+
 function contar() {
     let inicio = document.getElementById('txti');
     let fim = document.getElementById('txtf');
