de8cc564733923a3ddced9a4446a3e846d98cbce
rafaelamachadocamara <rafaelamachado.camara@yahoo.com.br>
2025-02-17 16:42:03 -0300

    criando uma estrutura for

---
+++ aula 16/ambiente.js
@@ -0,0 +1,19 @@
+/* var c = 1;    
+
+while (c <= 5) {
+    console.log(c);
+    c++;
+}
+
+do {
+    console.log(c);
+    c++;
+} while (c <= 5);
+
+*/
+
+// irei fazer o mesmo comportamento da função while porém utilizando o for
+
+for(var c =1; c <=5; c++){
+console.log(c);
+}
